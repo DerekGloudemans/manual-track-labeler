a2c6d7f80c9da41fa84ef70952dc859d020a8e59
Derek <derekgloudemans@gmail.com>
2021-09-01 13:50:38 -0500

    Resolve merge error

--- annotate/manual_annotator_3D.py
+++ annotate/manual_annotator_3D.py
@@ -600,6 +600,7 @@ class Annotator_3D():
                     bbox[1::2] += dy
                          
                     row[11:27] = bbox
+                    row[10] = "Manual"
                     
     
     def redraw(self,obj_idx,disp):
